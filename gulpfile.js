c67671c96db976719ea1be4b9f70c2e1b6899624
RayWarthog <choulei0112@hotmail.com>
2020-09-30 02:03:34 +0800

    Use gulp build pipeline

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const { src, dest, series } = require('gulp');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+
+const handlebars = require('gulp-compile-handlebars');
+const csvparse = require('csv-parse/lib/sync');
+const message_csv_file = 'src/messages.csv';
+const fs = require('fs').promises;
+const htmlmin = require('gulp-htmlmin');
+
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+function minifycss() {
+    return src('src/*.css')
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(dest('dist'));
+}
+
+async function build_html() {
+    let message_data = []
+
+    let content = await fs.readFile(message_csv_file);
+    let records = csvparse(content, { from_line: 2 });
+
+    records.map(
+        record => {
+            message_row = {
+                targetid: record[0],
+                name: record[1].trim(),
+                msg: record[2].trim()
+            };
+            message_data.push(message_row);
+        }
+    );
+
+    let template_data = {
+        messages: message_data
+    };
+    let htmlminoptions = {
+        minifyCSS: true,
+        minifyJS: true,
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+    }
+
+    return src('src/index.handlebars')
+        .pipe(handlebars(template_data))
+        .pipe(htmlmin(htmlminoptions))
+        .pipe(rename('index.html'))
+        .pipe(dest('.'));
+}
+
+function minifyjs() {
+    return src('src/*.js')
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(uglify())
+        .pipe(dest('dist'))
+}
+
+exports.build = series(minifycss, build_html, minifyjs);
